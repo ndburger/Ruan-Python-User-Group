8b699f5511d5d42ec8189581536c25aa33d4ffef
ndburger <nateburger60@gmail.com>
2018-11-16 18:09:07 -0600

    Delete shelfie1.py

--- w04c10/shelfie1.py
+++
@@ -1,6 +0,0 @@
-"""Demonstration of shelve to store Python objects."""
-import shelve
-shelfie = shelve.open('mydata')
-pets = ['Fluffy', 'Pookems', 'Killa']
-shelfie['pets'] = pets
-shelfie.close()
