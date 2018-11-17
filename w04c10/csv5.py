e79e9f4602fb7dea79811f5eae07ad452f5e6416
ndburger <nateburger60@gmail.com>
2018-11-16 18:07:52 -0600

    Delete csv5.py

--- w04c10/csv5.py
+++
@@ -1,5 +0,0 @@
-import csv
-csvFile = open('sample2.csv', 'w')
-sampleWriter = csv.writer(csvFile)
-sampleWriter.writerow(['Bob', 'Jones', '1234 Elm Street'])
-sampleWriter.writerow(['Jill', 'Green', '4321 Pine Avenue'])
