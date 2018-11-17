958a5450b9658b3cc4a9bb0d1cd6166a26a21729
ndburger <nateburger60@gmail.com>
2018-11-16 18:07:39 -0600

    Delete csv4.py

--- w04c10/csv4.py
+++
@@ -1,7 +0,0 @@
-"""Example reading of a csv file."""
-
-import csv
-csvFile = open('sample.csv')
-sampleReader = csv.reader(csvFile)
-for row in sampleReader:
-    print('Row #' + str(sampleReader.line_num) + ": " + str(row[0]))
