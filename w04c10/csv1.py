497c255f8fc4ef0831bd27287c51e85a5f4498eb
ndburger <nateburger60@gmail.com>
2018-11-16 18:07:21 -0600

    Delete csv1.py

--- w04c10/csv1.py
+++
@@ -1,7 +0,0 @@
-"""Example reading of a csv file."""
-
-import csv
-with open('sample.csv', newline='') as csvfile:
-    sampleReader = csv.reader(csvfile)
-    sampleData = list(sampleReader)
-    print(sampleData)
